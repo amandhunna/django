9baa0fd317932db19a7264246c8babcbc9dd8811
amandhunna <amandhunna.dev@gmail.com>
2022-06-28 15:02:47 +0530

    feat: worked with template variables

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, "index.html")
+    context = {
+        "var1": "aman" # key value should be string
+    }
+    return render(request, "index.html", context)
 
 def about(request):
     return HttpResponse("This is about page.")
