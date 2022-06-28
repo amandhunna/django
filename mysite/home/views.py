38891edb2c4fbdd970c9fcaa8e36fb8c99dbb97d
amandhunna <amandhunna.dev@gmail.com>
2022-06-28 14:43:52 +0530

    feat: used templates

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -1,14 +1,8 @@
-# from django.shortcuts import render, HttpResponse
-
-# # Create your views here.
-# def index(request):
-#     return HttpResponse("this is home page")
-
+from django.shortcuts import render
 from django.http import HttpResponse
 
-
 def index(request):
-    return HttpResponse("Hello, world. You're at the home index.")
+    return render(request, "index.html")
 
 def about(request):
     return HttpResponse("This is about page.")
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,7 +123,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Manual
-
 STATICFILES_DIRS = [
     BASE_DIR / "static",
     '/var/www/static/',
